853f1a29cf966fb8b607ee2525551172bf07c0f8
Bryan-Az <36939025+Bryan-Az@users.noreply.github.com>
2023-10-11 23:09:47 -0700

    initial attempt

--- serverless_start.sh
+++ serverless_start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-nvm install 6
-npm install -g serverless@beta
+nvm install 14
+npm install -g serverless
+# deploying to aws
 serverless create --template aws-nodejs
+serverless deploy
